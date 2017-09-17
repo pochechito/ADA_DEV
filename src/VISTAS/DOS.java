694a265c44834e7135433d97e8a3a012a3a8e174
Bosque popular <>
2017-09-17 06:32:15 -0500

    Crea una clase nueva

---
+++ src/VISTAS/DOS.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VISTAS;
+
+/**
+ *
+ * @author LCT
+ */
+public class DOS {
+    //Esta clase fue creada en el bosque popular el prado como prueba.
+}
